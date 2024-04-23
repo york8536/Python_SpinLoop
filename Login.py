04ca4bb3f936a1f7edc605065dca9416eb402dd4
york8536 <york8536@gmail.com>
2024-04-23 14:20:25 +0800

    20240423
    implicitly_wait

--- Login.py
+++ Login.py
@@ -3,18 +3,18 @@ import json
 
 
 
-# r = requests.post('https://qphoenix.1161023.lol/api/v1/login', json = data_login)
+# r = requests.post('https://sphoenix.089453.fun/api/v1/login', json = data_login)
 # print(r.status_code)
 # print(r.content)
-for id in range(10):
+for id in range(1):
     data_login = { # 參數設定
-        "SiteID": "qat",
-        "DeviceID": f"qatest_1219_{id}",
+        "SiteID": "str",
+        "DeviceID": f"qatest_01111_{id}",
         "TPType": 1,
-        "PlatID": 3, # PlatID   1:android   2:ios   3:web
+        "PlatID": 2, # PlatID   1:android   2:ios   3:web
         "Screen": "QATest"
     }
-    r = requests.post('https://qphoenix.1161023.lol/api/v1/login', json = data_login)
+    r = requests.post('https://sphoenix.089453.fun/api/v1/login', json = data_login)
     print(r)
-    # print(r.content)    
+    print(r.content)    
 print('done')
